92e5d35ce7d3283947b643b09afa48664bc5d308
chmueedzafar <chmueedzafar@gmail.com>
2024-09-03 19:19:56 +0500

    first commit

---
+++ Personal Portfolio/my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element = {<Home />} />
+      <Route path="/project" element = {<Project />} />
+      <Route path="/about" element = {<About />} />
+      <Route path="/contact" element = {<Contact />} />
+
+    </Routes>
+    </>
+  );
+}
+
+export default App;
