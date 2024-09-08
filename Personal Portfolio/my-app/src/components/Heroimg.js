ee9bd23bb153c814c9beb17a08ed7769f7447cf3
chmueedzafar <chmueedzafar@gmail.com>
2024-09-08 18:57:07 +0500

    final last commit

--- Personal Portfolio/my-app/src/components/Heroimg.js
+++ Personal Portfolio/my-app/src/components/Heroimg.js
@@ -1,25 +1,24 @@
-import "./HeroimgStyle.css";
-import React from 'react'
-import IntroImg from "../assets/scott-graham-5fNmWej4tAA-unsplash.jpg";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './HeroimgStyle.css';
+import IntroImg from '../assets/scott-graham-5fNmWej4tAA-unsplash.jpg';
+
 const Heroimg = () => {
   return (
     <div className="hero">
-        <div className="mask">
-            <img className="into-img" src={IntroImg} alt="introimg"/>
-            <div className="content">
-                <p>HI, I'M A FREELANCER.</p>
-                <h1>React Developer</h1>
-                <div>
-                    <Link to="/Project" className="btn">Projects</Link>
-                    <Link to="/contact" className="btn-light">Contact</Link>
-
-                </div>
-
-            </div>
-        </div>  
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="intro" />
+        <div className="content">
+          <p>HI, I'M A FREELANCER.</p>
+          <h1>React Developer</h1>
+          <div>
+            <Link to="/Contact" className="btn">Contact</Link>
+            <Link to="/Project" className="btn-light">Project</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Heroimg
+export default Heroimg;
